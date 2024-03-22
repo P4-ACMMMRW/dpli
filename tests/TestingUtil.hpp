dcee7e75a909e7f1e3521d2a3ccbffb253660b73
MadsVind <madsvind7@gmail.com>
2024-03-22 19:00:52 +0100

    Refactor visit methods in TestingUtil.hpp

--- tests/TestingUtil.hpp
+++ tests/TestingUtil.hpp
@@ -24,30 +24,98 @@ public:
         return visitChildren(node);
     }
 
-    virtual std::any visitStm(dplgrammar::DplParser::StmContext *ctx) override {
-        std::cout << "Stm " + ctx->getText() + "\n" << std::endl;
-        return visitChildren(ctx);
+    virtual std::any visitStm(dplgrammar::DplParser::StmContext *node) override {
+        std::cout << "Stm " + node->getText() + "\n" << std::endl;
+        return visitChildren(node);
     }
 
-    virtual std::any visitCtrlstm(dplgrammar::DplParser::CtrlstmContext *ctx) override {
-        std::cout << "Ctrlstm " + ctx->getText() + "\n" << std::endl;
-        return visitChildren(ctx);
+    virtual std::any visitCtrlstm(dplgrammar::DplParser::CtrlstmContext *node) override {
+        std::cout << "Ctrlstm " + node->getText() + "\n" << std::endl;
+        return visitChildren(node);
     }
 
-    virtual std::any visitLoopstm(dplgrammar::DplParser::LoopstmContext *ctx) override {
-        std::cout << "Loopstm " + ctx->getText() + "\n" << std::endl;
-        return visitChildren(ctx);
+    virtual std::any visitLoopstm(dplgrammar::DplParser::LoopstmContext *node) override {
+        std::cout << "Loopstm " + node->getText() + "\n" << std::endl;
+        return visitChildren(node);
     }
 
-    virtual std::any visitFlowstm(dplgrammar::DplParser::FlowstmContext *ctx) override {
-        std::cout << "Flowstm " + ctx->getText() + "\n" << std::endl;
-        return visitChildren(ctx);
+    virtual std::any visitFlowstm(dplgrammar::DplParser::FlowstmContext *node) override {
+        std::cout << "Flowstm " + node->getText() + "\n" << std::endl;
+        return visitChildren(node);
     }
 
-    virtual std::any visitAssignstm(dplgrammar::DplParser::AssignstmContext *ctx) override {
+    virtual std::any visitAssignstm(dplgrammar::DplParser::AssignstmContext *node) override {
         
-        std::cout << "Assignstm " + ctx->getText() + "\n" << std::endl;
-        return visitChildren(ctx);
+        std::cout << "Assignstm " + node->getText() + "\n" << std::endl;
+        return visitChildren(node);
+    }
+
+    virtual std::any visitExpr(dplgrammar::DplParser::ExprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitArthexpr(dplgrammar::DplParser::ArthexprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitBoolexpr(dplgrammar::DplParser::BoolexprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitUnaryexpr(dplgrammar::DplParser::UnaryexprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitTable(dplgrammar::DplParser::TableContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitTabledec(dplgrammar::DplParser::TabledecContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitTablecall(dplgrammar::DplParser::TablecallContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitList(dplgrammar::DplParser::ListContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitListdec(dplgrammar::DplParser::ListdecContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitListcall(dplgrammar::DplParser::ListcallContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitProcdec(dplgrammar::DplParser::ProcdecContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitProccall(dplgrammar::DplParser::ProccallContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitArgs(dplgrammar::DplParser::ArgsContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitParams(dplgrammar::DplParser::ParamsContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitCompareopr(dplgrammar::DplParser::CompareoprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitJunctionopr(dplgrammar::DplParser::JunctionoprContext *node) override {
+        return visitChildren(node);
+    }
+
+    virtual std::any visitLiteral(dplgrammar::DplParser::LiteralContext *node) override {
+        return visitChildren(node);
     }
 
     // Override other visit methods as needed...
