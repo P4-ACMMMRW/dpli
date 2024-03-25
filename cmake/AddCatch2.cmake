1f1c368731a096036d87700813e2a93064b91c04
mtygesen <mikkel.tygesen@gmail.com>
2024-03-25 14:30:04 +0100

    workflow ttest

--- cmake/AddCatch2.cmake
+++ cmake/AddCatch2.cmake
@@ -6,7 +6,6 @@ FetchContent_Declare(
     GIT_TAG v3.5.3
 )
 
-set(CMAKE_CXX_COMPILER "${CMAKE_CXX_COMPILER}")
-
 FetchContent_MakeAvailable(Catch2)
 list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
+set_property(TARGET Catch2WithMain PROPERTY CXX_STANDARD 17)
