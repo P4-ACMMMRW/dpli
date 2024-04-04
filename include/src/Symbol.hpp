7a08d3f51f5b51676c87b9527a8a8908ac1eb576
mtygesen <mikkel.tygesen@gmail.com>
2024-04-04 23:33:29 +0200

    Setup linting script to run clang-format & clang-tidy locally

--- include/src/Symbol.hpp
+++ include/src/Symbol.hpp
@@ -2,19 +2,20 @@
 #define SYMBOL_HPP
 
 #include <string>
+#include <utility>
 
 namespace dplsrc {
-    class Symbol {
-        public:
-            Symbol(std::string val) : val(val) {}
-            Symbol(std::string id, std::string val) : id(id), val(val) {}
-            std::string getId() const { return id; }
-            std::string getVal() const { return val; }
-        
-        private:
-            std::string id;
-            std::string val;
-    };
-}
+class Symbol {
+public:
+    Symbol(std::string val) : val(std::move(val)) {}
+    Symbol(std::string id, std::string val) : id(std::move(id)), val(std::move(val)) {}
+    std::string getId() const { return id; }
+    std::string getVal() const { return val; }
+
+private:
+    std::string id;
+    std::string val;
+};
+}  // namespace dplsrc
 
 #endif
--- include/src/SymbolTable.hpp
+++ include/src/SymbolTable.hpp
@@ -1,25 +1,25 @@
 #ifndef SYMBOLTABLE_HPP
 #define SYMBOLTABLE_HPP
 
-#include <string>
 #include <stack>
-#include <unordered_map>
 #include <stdexcept>
+#include <string>
+#include <unordered_map>
 
 #include "Symbol.hpp"
 
 namespace dplsrc {
-    class SymbolTable {
-        public:
-            void bind(Symbol s);
-            Symbol lookup(Symbol s);
-            void enter();
-            void exit();
+class SymbolTable {
+public:
+    void bind(Symbol s);
+    Symbol lookup(Symbol s);
+    void enter();
+    void exit();
 
-        private:
-            std::stack<Symbol> table;
-            std::unordered_map<std::string, Symbol> symbolLookupTable;
-    };
-}
+private:
+    std::stack<Symbol> table;
+    std::unordered_map<std::string, Symbol> symbolLookupTable;
+};
+}  // namespace dplsrc
 
 #endif
