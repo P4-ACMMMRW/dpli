aaf56fdbeff7385accfef3b4efbb71a277774c96
mtygesen <mikkel.tygesen@gmail.com>
2024-04-12 16:57:28 +0200

    Removed virtual

--- include/src/ast/AstBuilder.hpp
+++ include/src/ast/AstBuilder.hpp
@@ -21,43 +21,43 @@ class AstBuilder : public DplParserBaseVisitor {
     AstBuilder(DplParser *parser, DplLexer *lexer)
         : parser(parser), lexer(lexer), root(nullptr), currentNode(nullptr) {}
 
-    virtual antlrcpp::Any visitProg(DplParser::ProgContext *parseNode) override;
-    virtual antlrcpp::Any visitBlock(DplParser::BlockContext *parseNode) override;
+    antlrcpp::Any visitProg(DplParser::ProgContext *parseNode) override;
+    antlrcpp::Any visitBlock(DplParser::BlockContext *parseNode) override;
 
     // Declarations
-    virtual antlrcpp::Any visitStms(DplParser::StmsContext *parseNode) override;
-    virtual antlrcpp::Any visitProcdec(DplParser::ProcdecContext *parseNode) override;
-    virtual antlrcpp::Any visitAssignstm(DplParser::AssignstmContext *parseNode) override;
-    virtual antlrcpp::Any visitReturnstm(DplParser::ReturnstmContext *parseNode) override;
+    antlrcpp::Any visitStms(DplParser::StmsContext *parseNode) override;
+    antlrcpp::Any visitProcdec(DplParser::ProcdecContext *parseNode) override;
+    antlrcpp::Any visitAssignstm(DplParser::AssignstmContext *parseNode) override;
+    antlrcpp::Any visitReturnstm(DplParser::ReturnstmContext *parseNode) override;
 
     // IF-ELSE
-    virtual antlrcpp::Any visitIfstm(DplParser::IfstmContext *parseNode) override;
-    virtual antlrcpp::Any visitElsestm(DplParser::ElsestmContext *parseNode) override;
+    antlrcpp::Any visitIfstm(DplParser::IfstmContext *parseNode) override;
+    antlrcpp::Any visitElsestm(DplParser::ElsestmContext *parseNode) override;
 
     // While
-    virtual antlrcpp::Any visitWhilestm(DplParser::WhilestmContext *parseNode) override;
+    antlrcpp::Any visitWhilestm(DplParser::WhilestmContext *parseNode) override;
 
     // Expressions  The expressions isn't correctly sequenced
-    virtual antlrcpp::Any visitJuncexpr(DplParser::JuncexprContext *parseNode) override;
-    virtual antlrcpp::Any visitNotexpr(DplParser::NotexprContext *parseNode) override;
-    virtual antlrcpp::Any visitCompexpr(DplParser::CompexprContext *parseNode) override;
-    virtual antlrcpp::Any visitArthexpr(DplParser::ArthexprContext *parseNode) override;
+    antlrcpp::Any visitJuncexpr(DplParser::JuncexprContext *parseNode) override;
+    antlrcpp::Any visitNotexpr(DplParser::NotexprContext *parseNode) override;
+    antlrcpp::Any visitCompexpr(DplParser::CompexprContext *parseNode) override;
+    antlrcpp::Any visitArthexpr(DplParser::ArthexprContext *parseNode) override;
 
     // Terms
-    virtual antlrcpp::Any visitTerminal(tree::TerminalNode *node) override;
-    virtual antlrcpp::Any visitList(DplParser::ListContext *parseNode) override;
-    virtual antlrcpp::Any visitTable(DplParser::TableContext *parseNode) override;
-    virtual antlrcpp::Any visitColumn(DplParser::ColumnContext *parseNode) override;
-    virtual antlrcpp::Any visitTerm(DplParser::TermContext *parseNode) override;
-    virtual antlrcpp::Any visitSubscript(DplParser::SubscriptContext *parseNode) override;
-    virtual antlrcpp::Any visitIndex(DplParser::IndexContext *parseNode) override;
-    virtual antlrcpp::Any visitHeaderindex(DplParser::HeaderindexContext *parseNode) override;
-    virtual antlrcpp::Any visitFiltering(DplParser::FilteringContext *parseNode) override;
+    antlrcpp::Any visitTerminal(tree::TerminalNode *node) override;
+    antlrcpp::Any visitList(DplParser::ListContext *parseNode) override;
+    antlrcpp::Any visitTable(DplParser::TableContext *parseNode) override;
+    antlrcpp::Any visitColumn(DplParser::ColumnContext *parseNode) override;
+    antlrcpp::Any visitTerm(DplParser::TermContext *parseNode) override;
+    antlrcpp::Any visitSubscript(DplParser::SubscriptContext *parseNode) override;
+    antlrcpp::Any visitIndex(DplParser::IndexContext *parseNode) override;
+    antlrcpp::Any visitHeaderindex(DplParser::HeaderindexContext *parseNode) override;
+    antlrcpp::Any visitFiltering(DplParser::FilteringContext *parseNode) override;
 
-    virtual antlrcpp::Any visitUnaryexpr(DplParser::UnaryexprContext *parseNode) override;
-    virtual antlrcpp::Any visitProccall(DplParser::ProccallContext *parseNode) override;
-    virtual antlrcpp::Any visitArgs(DplParser::ArgsContext *parseNode) override;
-    virtual antlrcpp::Any visitParams(DplParser::ParamsContext *parseNode) override;
+    antlrcpp::Any visitUnaryexpr(DplParser::UnaryexprContext *parseNode) override;
+    antlrcpp::Any visitProccall(DplParser::ProccallContext *parseNode) override;
+    antlrcpp::Any visitArgs(DplParser::ArgsContext *parseNode) override;
+    antlrcpp::Any visitParams(DplParser::ParamsContext *parseNode) override;
 
     AstNode *getRoot();
 };
