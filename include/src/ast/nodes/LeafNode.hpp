1f138d0c308b56f58299a767cd38bccd3dd278c5
MegaScooper <carl@madsdore.dk>
2024-04-23 16:55:17 +0200

    hilfe stiefbruder

--- include/src/ast/nodes/LeafNode.hpp
+++ include/src/ast/nodes/LeafNode.hpp
@@ -1,9 +1,12 @@
 #ifndef LEAFNODE_HPP
 #define LEAFNODE_HPP
 
+#include <vector>
 #include <AstNode.hpp>
 #include <Symbol.hpp>
 #include <SymbolType.hpp>
+#include <memory>
+
 
 using namespace dplsrc;
 
@@ -11,11 +14,14 @@ class LeafNode : public AstNode {
    public:
     LeafNode(std::shared_ptr<AstNode> parent) { AstNode::setParent(std::move(parent)); }
 
-    void setType(const dplsrc::SymbolType leafType) { this->leafType = leafType; }
+    void setEvaluatedType(const dplsrc::SymbolType leafType) { this->leafType = leafType; }
 
-    dplsrc::SymbolType getType() const { 
+    const dplsrc::SymbolType &getEvaluatedType() const { 
         if (!leafType.is(SymbolSuperType::TYPE_INVALID))
             return leafType; 
+        if (children.size() != 1)
+            throw std::runtime_error("Cannot deduce evaluted type of symbol");
+        return std::static_pointer_cast<LeafNode>(children.front())->getEvaluatedType();
     }
 
     void addChild([[maybe_unused]] std::shared_ptr<AstNode> child) override;
@@ -24,10 +30,12 @@ class LeafNode : public AstNode {
 
     void accept(std::shared_ptr<AstVisitor> visitor) override;
 
-
-
+    
+    AstNode *parent;
+    std::vector<AstNode *> children;
    private: 
     dplsrc::SymbolType leafType = SymbolSuperType::TYPE_INVALID;
+    
 };
 
 #endif
