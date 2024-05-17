a0dc367d0da737e45959f1a8304a42c7d1af0b7a
mtygesen <mikkel.tygesen@gmail.com>
2024-05-17 13:53:24 +0200

    linter.sh fix

--- include/tests/TestingUtil.hpp
+++ include/tests/TestingUtil.hpp
@@ -1,19 +1,18 @@
 #ifndef TEST_HPP
 #define TEST_HPP
 
-#include <catch2/catch_test_macros.hpp>
-#include <fstream>
-#include <filesystem>
-#include <iostream>
-#include <antlr4-runtime.h>
 #include <DplLexer.h>
 #include <DplParser.h>
 #include <DplParserBaseVisitor.h>
+#include <antlr4-runtime.h>
+
 #include <AstTestVisitor.hpp>
 #include <Evaluator.hpp>
-#include <fstream>
+#include <catch2/catch_test_macros.hpp>
 #include <cstdio>
-
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 
 #define LEXER_TEST(name) TEST_CASE("Lexer -- " #name, "[lexer]")
 #define PARSER_TEST(name) TEST_CASE("Parser -- " #name, "[parser]")
@@ -24,34 +23,34 @@
 #define EVAL_EXCEPTION_TEST(name) TEST_CASE("Eval Exception Test --" #name, "[eval_exception_test]")
 
 namespace dplutil {
-    class TestVisitor : public dplgrammar::DplParserBaseVisitor {
-        private:
-            dplgrammar::DplParser* parser; // Add a parser member
-            std::vector<size_t> expectedTreeNodes;
+class TestVisitor : public dplgrammar::DplParserBaseVisitor {
+   private:
+    dplgrammar::DplParser* parser;  // Add a parser member
+    std::vector<size_t> expectedTreeNodes;
 
-        public:
-            TestVisitor(dplgrammar::DplParser* parser, std::vector<size_t> expectedTreeNodes) 
-                : parser(parser), expectedTreeNodes(expectedTreeNodes) {} 
+   public:
+    TestVisitor(dplgrammar::DplParser* parser, std::vector<size_t> expectedTreeNodes)
+        : parser(parser), expectedTreeNodes(expectedTreeNodes) {}
 
-            void printTokens(int ruleIndex);
-            void testNode(antlr4::ParserRuleContext *ctx);
-            antlrcpp::Any visitChildren(antlr4::tree::ParseTree *tree) override;
-    };
+    void printTokens(int ruleIndex);
+    void testNode(antlr4::ParserRuleContext* ctx);
+    antlrcpp::Any visitChildren(antlr4::tree::ParseTree* tree) override;
+};
 
-    class TestingUtil {
-        public:
-            static void testTokens(std::string testFileName, std::vector<size_t> expectedTokenTypes);
-            static void testParser(std::string testFileName, std::vector<size_t> expectedTreeNodes);
-            static void testAst(std::string testFileName, std::vector<size_t> expectedTreeNodes);
-            static void testEval(std::string testFileName, 
-                                 std::vector<std::pair<std::string, dplsrc::Value>> expectedVarVec,
-                                 std::vector<std::string> expectedOutputLines);
-            static void testEvalException(std::string testFileName);
+class TestingUtil {
+   public:
+    static void testTokens(std::string testFileName, std::vector<size_t> expectedTokenTypes);
+    static void testParser(std::string testFileName, std::vector<size_t> expectedTreeNodes);
+    static void testAst(std::string testFileName, std::vector<size_t> expectedTreeNodes);
+    static void testEval(std::string testFileName,
+                         std::vector<std::pair<std::string, dplsrc::Value>> expectedVarVec,
+                         std::vector<std::string> expectedOutputLines);
+    static void testEvalException(std::string testFileName);
 
-        private:
-            static constexpr std::string_view exampleLocation = "../../docs/examples/"; 
-            static constexpr std::string_view exceptionExampleLocation = "../../docs/examples/exceptions/"; 
-    };
-}
+   private:
+    static constexpr std::string_view exampleLocation = "../../docs/examples/";
+    static constexpr std::string_view exceptionExampleLocation = "../../docs/examples/exceptions/";
+};
+}  // namespace dplutil
 
 #endif
